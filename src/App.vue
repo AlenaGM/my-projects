9c09654462c2c2a60707310ed73a98feff5cb071
AlenaGM <alena.guillaume4@gmail.com>
2023-08-14 14:29:30 +0200

    setup: added and tested gsap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+import { onMounted, onUnmounted, ref } from 'vue'
+import gsap from 'gsap'
+
+const main = ref()
+let tl
+let ctx
+
+function toggleTimeline() {
+  tl.reversed(!tl.reversed())
+}
+
+onMounted(() => {
+  ctx = gsap.context((self) => {
+    const boxes = self.selector('.box')
+    tl = gsap
+      .timeline()
+      .to(boxes[0], { x: 120, rotation: 360 })
+      .to(boxes[1], { x: -120, rotation: -360 }, '<')
+      .to(boxes[2], { y: -166 })
+      .reverse()
+  }, main.value) // <- Scope!
+})
+
+onUnmounted(() => {
+  ctx.revert() // <- Easy Cleanup!
+})
 </script>
 
 <template>
@@ -9,7 +36,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <section class="boxes-container" ref="main">
+        <h1>Use the button to toggle a Timeline</h1>
+        <div>
+          <button @click="toggleTimeline">Toggle Timeline</button>
+        </div>
+        <div class="box">Box 1</div>
+        <div class="box">Box 2</div>
+        <div class="box">Box 3</div>
+      </section>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
