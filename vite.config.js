478438baca73dcbe5053719d349aabc71ab7e5d6
AlenaGM <alena.guillaume4@gmail.com>
2023-09-21 01:30:48 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/alenawebdev.netlify.app/',
   plugins: [vue()],
   resolve: {
     alias: {
