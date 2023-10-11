469fa59d78622ecfc2abb50bb7c7b4e1447fb97a
AlenaGM <alena.guillaume4@gmail.com>
2023-10-11 22:39:36 +0200

    background change test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup>
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
-import About from '@/components/About.vue'
-import Skills from '@/components/Skills.vue'
-import Work from '@/components/Work.vue'
-import SocialWidget from '@/components/ui/SocialWidget.vue'
-</script>
-
 <template>
   <Header />
   <main class="main">
@@ -17,3 +8,76 @@ import SocialWidget from '@/components/ui/SocialWidget.vue'
   </main>
   <Footer />
 </template>
+
+<script setup>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import About from '@/components/About.vue'
+import Skills from '@/components/Skills.vue'
+import Work from '@/components/Work.vue'
+import SocialWidget from '@/components/ui/SocialWidget.vue'
+
+import { onMounted } from 'vue'
+
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
+
+onMounted(() => {
+  //Work section: background color change
+  gsap.set('.work', {
+    background: 'var(--color-white)'
+  })
+
+  gsap.set('.work h2', {
+    color: 'var(--color-secondary)'
+  })
+
+  const tlBg = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.work',
+      start: 'top 50%',
+      end: 'bottom 50%',
+      duration: 0.2,
+      toggleActions: 'play reverse play reverse'
+    }
+  })
+
+  tlBg
+    .to(
+      'body',
+      {
+        background: 'var(--color-gray)'
+      },
+      '<'
+    )
+    .to(
+      '.work h2',
+      {
+        color: 'var(--color-primary)'
+      },
+      '<'
+    )
+    .to(
+      '.work',
+      {
+        backgroundColor: 'transparent'
+      },
+      '<'
+    )
+    .to(
+      '.work p',
+      {
+        color: 'var(--color-white)'
+      },
+      '<'
+    )
+    .to(
+      '.contact h2',
+      {
+        color: 'var(--color-primary)'
+      },
+      '<'
+    )
+})
+</script>
---
+++ src/gsap.js
@@ -0,0 +1,5 @@
+//export * from 'gsap'
+//export * from 'gsap/ScrollTrigger'
+//import { gsap } from 'gsap'
+//import { ScrollTrigger } from 'gsap/ScrollTrigger'
+//gsap.registerPlugin(ScrollTrigger)
